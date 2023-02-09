c00bcf34714d008632dd938cdfa5a705419be5eb
nirrjhorr <nirjhor456@gmail.com>
2023-02-09 20:26:36 +0600

    added products and  page routing completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
+// import data from './data';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Agromart</a>
-      </header>
-      <main>list product</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          {/* <Link to="/">Agromart</Link> */}
+          <Link to="/">Agromart</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
